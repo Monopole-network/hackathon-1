7e32a535c6605a73c2583c1284ce15f0f8745716
wpiron10 <w.piron.perso@gmail.com>
2023-09-21 09:39:40 +0200

    william : handle errors to deploy in netlify

--- components/SubFilter.tsx
+++ components/SubFilter.tsx
@@ -1,50 +1,50 @@
 import React from "react";
-import { Button, Card, CardBody, CardHeader, Checkbox, Collapse, Stack } from '@chakra-ui/react';
+import { Button, Card, CardBody, CardHeader, Checkbox, Collapse, Stack } from "@chakra-ui/react";
 import { ChevronDownIcon, ChevronUpIcon, RepeatIcon, SearchIcon } from "@chakra-ui/icons";
 
 type SubFilterProps = {
-  subfiltersArray: string[];
+	subfiltersArray: string[];
 };
 
 const SubFilter: React.FC<SubFilterProps> = ({ subfiltersArray }) => {
-  const [isDisplayed, setIsDisplayed] = React.useState(false);
+	const [isDisplayed, setIsDisplayed] = React.useState(false);
 
-  const handleSubFilters: any = () => {
-    setIsDisplayed(!isDisplayed);
-  };
+	const handleSubFilters: any = () => {
+		setIsDisplayed(!isDisplayed);
+	};
 
-  return (
-    <>
-      <Stack direction='row' align='center'>
-        <Button
-          colorScheme='blue'
-          onClick={handleSubFilters}
-          rightIcon={isDisplayed ? <ChevronUpIcon /> : <ChevronDownIcon />}
-        >
-          Filters
-        </Button>
-      </Stack>
-      <Collapse in={isDisplayed}>
-        <Card colorScheme="blue" style={{marginTop: 20}}>
-          <CardBody>
-            <Stack direction='column' align='left'>
-              {subfiltersArray.map((subfilter: any) => (
-                <Checkbox>{subfilter}</Checkbox>
-              ))}
-            </Stack>
-            <Stack style={{marginTop: 20}} direction='row' align='left'>
-              <Button variant="outline" onClick={handleSubFilters}>
-                <SearchIcon />
-              </Button>
-              <Button variant="outline" onClick={handleSubFilters}>
-                <RepeatIcon />
-              </Button>
-            </Stack>
-          </CardBody>
-      </Card>
-      </Collapse>
-    </>
-  );
-}
+	return (
+		<>
+			<Stack direction="row" align="center">
+				<Button
+					colorScheme="blue"
+					onClick={handleSubFilters}
+					rightIcon={isDisplayed ? <ChevronUpIcon /> : <ChevronDownIcon />}
+				>
+					Filters
+				</Button>
+			</Stack>
+			<Collapse in={isDisplayed}>
+				<Card colorScheme="blue" style={{ marginTop: 20 }}>
+					<CardBody>
+						<Stack direction="column" align="left">
+							{subfiltersArray.map((subfilter: any) => (
+								<Checkbox key={subfilter}>{subfilter}</Checkbox>
+							))}
+						</Stack>
+						<Stack style={{ marginTop: 20 }} direction="row" align="left">
+							<Button variant="outline" onClick={handleSubFilters}>
+								<SearchIcon />
+							</Button>
+							<Button variant="outline" onClick={handleSubFilters}>
+								<RepeatIcon />
+							</Button>
+						</Stack>
+					</CardBody>
+				</Card>
+			</Collapse>
+		</>
+	);
+};
 
 export default SubFilter;
