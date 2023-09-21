5f778de1b2e83e62e701fb9f16fbce1a7dbd0c8c
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2023-09-21 17:07:55 +0200

    Fixes

--- components/Filter.tsx
+++
@@ -1,8 +0,0 @@
-import { SmallCloseIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
-
-const Filter = (active: boolean, label: string) => {
-  return ({ active } ? <Button>{label}</Button> : <Button>{label}</Button>)
-}
-
-export default Filter;
